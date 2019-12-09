79daa39edccaeecc88338f8b0f464df1b18f9908
emilebourban <emilebourban@gmail.com>
2019-12-09 10:58:40 +0100

    deleted useless functions

--- project.py
+++ project.py
@@ -11,59 +11,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def clean_cwd(*args):
-    """Removes most of the simulation files from the current directory"""
-
-    # Generator of the files generated for each runs
-    mv_files = (file for file in os.listdir() if file.endswith('.vtk')
-                or file.endswith('.dat')
-                or file.startswith('eeldata')
-                or file.startswith('dmpcis.')
-                or file.startswith('dmpcls.')
-                or file.startswith('dmpchs.')
-                or file.startswith('dmpcas.')
-                or file.endswith('_sim'))
-
-    # If no argument were given to name the folder, we will default to date and time to name the folder and archive the simulation files.
-    folder_loc = '_'.join(list(map(str, args))) if args is not None else time.strftime("%Y.%m.%d-%H%M%S")
-    os.makedirs(folder_loc, exist_ok=True)
-
-    for file in mv_files:
-        try:
-            shutil.move(file, os.path.join(folder_loc, file))
-        except:
-            print(f"Failed to move {file}")
-            raise
-        
-    print('')
-
-
-def archive_results(seeds):
-    """ Moves all the resulting folders in a single one, in the 'results' folder, by name. """
-
-    # Moves all created folder and result files in a single folder
-    datetime = time.strftime("%Y.%m.%d-%H%M%S")
-    try:
-        os.makedirs(f"Results/{datetime}")
-    except:
-        print("Failed to create folder Results/{:s}".format(datetime))
-        raise
-
-    try:
-        shutil.move('results.log', f"Results/{datetime:s}/results.log")        
-    except:
-        print("Failed to move results.log")
-
-    for seed in seeds:
-        mv_folders = (folder for folder in os.listdir() if folder.endswith(str(seed)))
-        for folder in mv_folders:
-            try:
-                shutil.move(folder, f"Results/{datetime}/{folder}")
-            except:
-                print(f"Failed to move {folder}")
-                raise
-
-
 def change_input(filename, frac_Lp, seed=None, time=30000):
     """Creates a new dmpci.ms_sim with updated number fraction values (cf dpd doc)"""
 
@@ -127,72 +74,7 @@ def change_input(filename, frac_Lp, seed=None, time=30000):
 
                 else:
                     wf.write(line)
-                    
-
-def get_lengths(filename, polymer, time, means, stds):
-    """Parses 'as' file to get the EE length mean and std, time allows to return the correct ee lengths based on dmpci analysis time"""
-
-    time_toggle = False
-    with open(filename, 'rt') as f:
-        for line in f:
-            if line.startswith(f"Time = {time}"):
-                time_toggle = True
-
-            if line.startswith(f"{polymer} EE distance") and time_toggle:
-                line = next(f)
-                means.append(float(line.strip().split()[0]))
-                stds.append(float(line.strip().split()[1]))
-                break
-
-        else:
-            raise EOFError(f"No {polymer} EE distance found")
-        
-
-def get_length_time_serie(file_ext):
-    """"""
-
-    n_polymers = get_n_polymers('.ms_sim')
-    times, lenghts = [], []
-    with open(f'dmpcas{file_ext}', 'rt') as f:
-        for line in f:
-
-            if line.startswith("Time"):
-                times.append(int(line.strip().split()[-1]))
-
-            if line.startswith("PEG EE distance"):
-                line = next(f)
-                lenghts.append(float(line.strip().split()[0]))
-
-    time_str = time.strftime("%H%M%S")
-
-    plt.figure(figsize=(10,10))
-    plt.plot(times, lenghts)
-    plt.ylim(0, None)
-    plt.xlabel('Time [ns]')
-    plt.ylabel('Mean Polymer EE length')
-    plt.savefig(f"{time_str:s}_{n_polymers:d}.png")
-
-    with open('log.log', 'at') as f:
-        defalut_out = sys.stdout
-        sys.stdout = f
-
-        print(n_polymers)
-        print(times)
-        print(lenghts)
-
-        sys.stdout = defalut_out
-
-
-def get_n_polymers(file_ext):
-
-    with open(f"dmpcis{file_ext}", 'rt') as f:
-        for line in f:
-            if line.startswith("  # of type 1"):
-                return int(line.strip().split()[-1])
-
-        else:
-            raise EOFError(f"Could not find number of polymers in 'dmpcis{file_ext}'")
-
+  
 
 def run_sim(params):
 
