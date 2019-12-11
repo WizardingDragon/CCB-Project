dae68bdfab138a80e40dcf2ba922f6a7db755507
WizardingDragon <jeremy.alexandre@epfl.ch>
2019-12-11 11:44:49 +0100

    Cleaned the repository, added files and folders to gitignore

--- Results/lipid6FracTest/lipid6_graph.py
+++ Results/lipid6FracTest/lipid6_graph.py
@@ -4,6 +4,7 @@ import os
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
+from itertools import cycle
 
 labels = []
 files = []
@@ -28,7 +29,11 @@ for i in range(len(labels)):
             
 x_axis = list(range(1000, 21000, 1000))
 
+from itertools import cycle
+lines = ["-","--","-.",":"]
+linecycler = cycle(lines)
+
 for label in labels:
-    plt.plot(x_axis[5:-1], labelValue[label][5:-1])
+    plt.plot(x_axis[5:-1], labelValue[label][5:-1], next(linecycler))
 plt.legend(labels)
 plt.show()
