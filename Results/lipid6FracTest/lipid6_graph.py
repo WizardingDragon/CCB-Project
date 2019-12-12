35a096563228065ae93bbee9fa15fa7021cf4cdc
WizardingDragon <jeremy.alexandre@epfl.ch>
2019-12-12 10:25:18 +0100

    New simulation for stabilising the bilayer with 6 lipids. Updated the graph functions.

--- Results/lipid6FracTest/lipid6_graph.py
+++ Results/lipid6FracTest/lipid6_graph.py
@@ -5,12 +5,21 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 from itertools import cycle
-print(os.system('pwd'))
+# Change the default pyplot parameters to put a readable size of text
+params = {'legend.fontsize': 'xx-large',
+          'figure.figsize': (15, 5),
+         'axes.labelsize': 'xx-large',
+         'axes.titlesize':'xx-large',
+         'xtick.labelsize':'xx-large',
+         'ytick.labelsize':'xx-large'}
+plt.rcParams.update(params)
+
+
 labels = []
 files = glob.glob('*/dmpcas.*')
 tmp_files = []
 for i in range(len(files)):
-    labels.append(str(files[i].strip().split('/')[0:1]))
+    labels.append(str(files[i].strip().split('/')[0:1]).strip().split('_')[1][:-2])
     tmp_files.append(str(str(files[i].split('/')[0]) + '/' + str(files[i].split('.')[-1]) + '.tmp'))
     if os.path.exists(tmp_files[i]):
         os.system(f'rm { tmp_files[i] }')
@@ -30,19 +39,24 @@ for i in range(len(files)):
             
 x_axis = list(range(1000, 21000, 1000))
 
-# print(labels[0])
-# plt.plot(x_axis[5:-1], labelValue["['1.125_-8349']"][5:-1])
-
-# plt.show()
-from itertools import cycle
+# Changes the line type for each lines
 lines = ["-","--","-.",":"]
 linecycler = cycle(lines)
 
+plt.figure(figsize=(12,8))
 for label in labels:
-    plt.plot(x_axis[5:-1], labelValue[label][5:-1], next(linecycler))
+    plt.plot(x_axis[5:-1], labelValue[label][5:-1], next(linecycler), linewidth=3)
 plt.legend(labels)
 plt.grid()
 plt.xlabel('Time')
 plt.ylabel('Bilayer Surface Tension')
+
 plt.show()
 
+plt.figure(figsize=(12,8))
+plt.plot(x_axis[5:-1],labelValue['1.125'][5:-1], 'k', x_axis[5:-1], labelValue['1.25'][5:-1], 'r')
+plt.grid()
+plt.legend(['1.125','1.25'])
+plt.xlabel('Time')
+plt.ylabel('Bilayer Surface Tension')
+plt.show()
