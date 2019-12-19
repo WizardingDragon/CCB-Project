4dd702f8a2a665a0d1014f8a2bd9df5a415417b8
WizardingDragon <jeremy.alexandre@epfl.ch>
2019-12-19 10:51:34 +0100

    Cleanup

--- Results/Archive/stabilizeLipid4_SLfail/irisProject.py
+++ Results/Archive/stabilizeLipid4_SLfail/irisProject.py
@@ -15,7 +15,7 @@ def change_input(filename, frac, seed=None, time=20000):
     frac_LT = 0.019994
 
     # Fraction of lipids in the simulation
-    frac_sl = 0#round(1.5/(32**3 *3), 7)
+    frac_sl = round(1.5/(32**3 *3), 7)
     frac_Lp = round(frac * frac_LT - frac_sl, 7)
     frac_w = round(1 - frac_sl - frac_Lp, 7)
 
@@ -77,25 +77,25 @@ def run_sim(params):
 
     os.mkdir(folder)
 
-    files = ['dmpci.lp10_eq', 'dpd-linux']
+    files = ['dmpci.lp6_eq', 'dpd-linux']
     for file in files:
         shutil.copy(file, folder+file)
 
-    change_input(f'{folder}dmpci.lp10_eq', params['frac'], params['seed'])
+    change_input(f'{folder}dmpci.lp6_eq', params['frac'], params['seed'])
 
     # Starts simulation and deletes the dpd linux file to reduce size when scp-ing folders between computers
-    os.system(f'cd {folder} && ./dpd-linux lp10_eq_sim && rm dpd-linux')
+    os.system(f'cd {folder} && ./dpd-linux lp6_eq_sim && rm dpd-linux')
 
 
 def main():
     # Fraction of lipid in the simulation volume andround them to 3 decimals with numpy.around()
-    frac = np.around(np.linspace(1,1.25,21), decimals=3)
+    frac = np.around(np.linspace(0.5,2,21), decimals=3)
     
     # Change FOLDER NAME BEFORE ADDING OTHER SEEDS!!!!!!!!!!!!
     np.random.seed(279)
     seeds = np.random.randint(-9999, -1000, size=1)
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    sims = [{'folder': f'lp10_{frac[i]:.7f}/', 'frac': frac[i], 'seed': seeds[j]} 
+    sims = [{'folder': f'lp6-sl_{frac[i]:.7f}/', 'frac': frac[i], 'seed': seeds[j]} 
             for i in range(len(frac)) for j in range(len(seeds))]
     
     print(sims)
